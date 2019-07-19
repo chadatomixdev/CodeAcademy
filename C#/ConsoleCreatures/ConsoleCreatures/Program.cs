e9adc831708a4b8e0db52f5e09070f2316814e68
sidchadbonthuys <chad.bonthuys@sidpayment.com>
2019-07-19 09:48:18 +0200

    --Console Creatures Freeform project

---
+++ C#/ConsoleCreatures/ConsoleCreatures/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ConsoleCreatures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // The purpose of this program is to build a creature using ASCII art
+            Console.WriteLine(" .-.");
+            Console.WriteLine("(o o)");
+            Console.WriteLine("| o |");
+            Console.WriteLine("|   |");
+            Console.WriteLine("'~~~'");
+            Console.WriteLine("CASPER THE FRIENDLY GHOST");
+
+            // Build the dancing man
+            Console.WriteLine("c_     ___");
+            Console.WriteLine("　 \\\\__|__|__");
+            Console.WriteLine("　　 \\( o_o)");
+            Console.WriteLine("　　　 > ~  >");
+            Console.WriteLine("　　　/ 　 / \\");
+            Console.WriteLine("　　 /　　/　 \\");
+            Console.WriteLine("　　 \\　 )　　c/");
+            Console.WriteLine("　　/　 /");
+            Console.WriteLine("　 /　/|");
+            Console.WriteLine("　(　( \\");
+            Console.WriteLine("　|　|  \\");
+            Console.WriteLine("　| / \\  )");
+            Console.WriteLine("　| |　) |");
+            Console.WriteLine(" /  )(_/");
+            Console.WriteLine("(_ /");
+        }
+    }
+}
