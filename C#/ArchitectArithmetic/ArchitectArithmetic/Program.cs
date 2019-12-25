3ee018110d53308c49c182d6491971ae8df878b3
Chad Bonthuys <chad@atomixdev.com>
2019-12-25 21:08:47 +0000

    Area calculation changes

--- C#/ArchitectArithmetic/ArchitectArithmetic/Program.cs
+++ C#/ArchitectArithmetic/ArchitectArithmetic/Program.cs
@@ -6,7 +6,28 @@ namespace ArchitectArithmetic
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var rectArea = Rectangle(4, 5);
+            var circArea = Circle(4);
+            var triArea = Triangle(10, 9);
+
+            Console.WriteLine($"Rectangle area of {rectArea}");
+            Console.WriteLine($"Circle area of {circArea}");
+            Console.WriteLine($"Triangle area of {triArea}");
+        }
+
+        private static double Rectangle(double length, double width)
+        {
+            return length * width;
+        }
+
+        public static double Circle(double radius)
+        {
+            return Math.PI * Math.Pow(radius,radius);
+        }
+
+        public static double Triangle(double bottom, double height)
+        {
+            return 0.5 * (bottom * height);
         }
     }
 }
