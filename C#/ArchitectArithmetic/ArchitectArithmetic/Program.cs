a767223802d3c4c4d2ec0a50a5f4fcc61a4c04cb
Chad Bonthuys <chad@atomixdev.com>
2019-12-27 12:45:10 +0000

    Final Changes to calculate the area for monuments using methods

--- C#/ArchitectArithmetic/ArchitectArithmetic/Program.cs
+++ C#/ArchitectArithmetic/ArchitectArithmetic/Program.cs
@@ -6,25 +6,81 @@ namespace ArchitectArithmetic
     {
         static void Main(string[] args)
         {
-            var rectArea = Rectangle(4, 5);
-            var circArea = Circle(4);
-            var triArea = Triangle(10, 9);
-
-            Console.WriteLine($"Rectangle area of {rectArea}");
-            Console.WriteLine($"Circle area of {circArea}");
-            Console.WriteLine($"Triangle area of {triArea}");
+            CalculateTotalCost();
         }
 
+        private static void CalculateTotalCost()
+        {
+            Console.WriteLine("What monument would you like to work with? Type T for Teotihuacan, TM for the Taj Mahal and GMM for the Great Mosque of Mecca");
+            var decision = Console.ReadLine();
+
+
+            switch (decision)
+            {
+                case "T":
+                    CalculateTeotihuacanCost();
+                    break;
+                case "TM":
+                    CalculateTajMahalCost();
+                    break;
+                case "GMM":
+                    CalculateGreatMosqueCost();
+                    break;
+                default:
+                    Console.WriteLine("Invalid monument selected");
+                    break;
+            }
+        }
+
+        private static void CalculateTeotihuacanCost()
+        {
+            var rectArea = Rectangle(2500, 1500);
+            var circArea = Circle(375);
+            var triArea = Triangle(500, 750);
+
+            var teotihuacanArea = rectArea + triArea + (circArea / 2);
+
+            var totalCost = teotihuacanArea * 180;
+            Console.WriteLine($"Total Cost of the ancient city of Teotihuacan in mexico is { Math.Round(totalCost) } pesos!");
+        }
+
+        private static void CalculateTajMahalCost()
+        {
+            var rectArea = Rectangle(90.5, 90.5);
+            var triArea = Triangle(24, 24);
+
+            var tajMahalArea = rectArea - (triArea * 4);
+
+            var totalCost = tajMahalArea * 180;
+            Console.WriteLine($"Total Cost of the Taj Mahal in Arga india is { Math.Round(totalCost) } pesos!");
+        }
+
+        private static void CalculateGreatMosqueCost()
+        {
+            var smallRectArea = Rectangle(106, 84);
+            var triArea = Triangle(84, 264);
+            var largeRect = Rectangle(390, 264);
+
+
+            var greatMosqueArea = largeRect - (smallRectArea * 2) - triArea;
+            var totalCost = greatMosqueArea * 180;
+
+            Console.WriteLine($"Total Cost of the Great Mosque in Mecca, Saudi Arabia is { Math.Round(totalCost) } pesos!");
+        }
+
+        //area=length×width
         private static double Rectangle(double length, double width)
         {
             return length * width;
         }
 
+        //area=π×radius^2
         public static double Circle(double radius)
         {
-            return Math.PI * Math.Pow(radius,radius);
+            return Math.PI * (Math.Pow(radius, 2));
         }
 
+        //area=0.5×bottom×height
         public static double Triangle(double bottom, double height)
         {
             return 0.5 * (bottom * height);
