5f2d7a78111edd5f642ff58c7f510f8aa6919fff
Chad Bonthuys <chad@atomixdev.com>
2019-12-30 12:16:53 +0000

    Working TrueorFalse

---
+++ C#/TrueorFalse/README.md
@@ -0,0 +1,14 @@
+# True or False?
+
+Use C# arrays and loops to create a true or false quiz.
+
+You’re taking an interactive quiz written in C#. Everything is going fine until you respond to this question:
+
+![alt text](https://codecademy-content.s3.amazonaws.com/courses/learn-c-sharp/lists-and-loops/error-example1.gif)
+
+This application wasn’t checking your input. When you used an unexpected format, it couldn’t ask the question again and marked your answer as wrong. (By botanical definition, eggplants really are berries!).
+
+In this project, you’ll build a C# program that presents a quiz the right way: using arrays and loops, it will check the format of user input and repeat the question if the format is incorrect. After the quiz is complete, it will check the user’s responses against the correct answers and present a score.
+
+![alt text](https://codecademy-content.s3.amazonaws.com/courses/learn-c-sharp/lists-and-loops/working-example3.gif)
+
---
+++ C#/TrueorFalse/TrueorFalse/TrueorFalse/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace TrueorFalse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to 'True or False?'\nPress Enter to begin:");
+            string entry = Console.ReadLine();
+            //Tools.SetUpInputStream(entry);
+
+            string[] questions = { "The earth is the fourth planet from the sun?", "The planet Venus has no moons?", "Jupiter is composed mostly of iron?", "The sun is a star of average size?", "A lunar eclipse occurs when the sun passes?" };
+            bool[] answers = { false, true, false, true, false };
+            bool[] responses = new bool[5];
+
+            if (questions.Length != answers.Length)
+            {
+                Console.WriteLine("Warning questions and answers do not match up.");
+            }
+
+            int askingIndex = 0;
+
+            foreach (var question in questions)
+            {
+                string input = "";
+                bool isBool;
+                bool inputBool;
+
+                Console.WriteLine(question);
+
+                input = Console.ReadLine();
+                isBool = bool.TryParse(input, out inputBool);
+
+                while (isBool == false)
+                {
+                    Console.WriteLine("Please respond with 'true' or 'false'");
+
+                    input = Console.ReadLine();
+                    isBool = bool.TryParse(input, out inputBool);
+                }
+
+                responses[askingIndex] = inputBool;
+                askingIndex++;
+            }
+
+
+            int scoringIndex = 0;
+            int score = 0;
+
+
+            foreach (var answer in answers)
+            {
+                bool response = responses[scoringIndex];
+
+                Console.WriteLine($"Input: { response } | Answer: { answer }");
+
+                if (answer == response)
+                {
+                    score++;
+                }
+
+                scoringIndex++;
+            }
+
+            Console.WriteLine($"You got { score } out of 5 correct!");
+        }
+    }
+}
