197bab120b942b1aa445e1a1273dee0de3023030
Chad Bonthuys <chad@atomixdev.com>
2020-01-12 18:45:34 +0000

    Completed project

---
+++ C#/TheObjectOfYourAffection/TheObjectOfYourAffection/TheObjectOfYourAffection/Profile.cs
@@ -0,0 +1,55 @@
+﻿namespace TheObjectOfYourAffection
+{
+    public class Profile
+    {
+        #region Fields
+
+        private string name;
+        private int age;
+        private string city;
+        private string country;
+        private string pronouns;
+        private string[] hobbies;
+
+        #endregion
+
+        #region Constructor
+
+        public Profile(string name, int age, string city, string country, string pronouns = "they/them")
+        {
+            this.name = name;
+            this.age = age;
+            this.city = city;
+            this.country = country;
+            this.pronouns = pronouns;
+        }
+
+        #endregion
+
+        #region Methods
+
+        public string ViewProfile()
+        {
+            string profile = "";
+            profile += $"Name: {name}";
+            profile += $" Age: {age}";
+            profile += $" City: {city}";
+            profile += $" Country: {country}";
+            profile += $" Pronouns: {pronouns}";
+
+            for (int i = 0; i < this.hobbies.Length; i++)
+            {
+                profile += $" Hobby {i + 1}: {hobbies[i]}";
+            }
+
+            return profile;
+        }
+
+        public void SetHobbies(string[] hobbies)
+        {
+            this.hobbies = hobbies;
+        }
+
+        #endregion
+    }
+}
---
+++ C#/TheObjectOfYourAffection/TheObjectOfYourAffection/TheObjectOfYourAffection/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace TheObjectOfYourAffection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Profile profile = new Profile("Sam Drakkila", 30, "New York", "USA", "he/him");
+            profile.SetHobbies(new string[] { "Listening to audiobooks and podcasts", "Playing rec sports like bowling and kickball", "Writing a speculative fiction novel", "Reading advice columns" });
+            Console.WriteLine(profile.ViewProfile());
+        }
+    }
+}
